2a851e51107e87dd2aaa83bad7f35703c3d3d4a9
CaptainHottub <80978904+CaptainHottub@users.noreply.github.com>
2022-05-26 15:49:25 -0700

    custom_keyboard now has 1 dictionary of keycodes

--- MacroV2/custom_keyboard.py
+++ MacroV2/custom_keyboard.py
@@ -74,334 +74,230 @@ user32.SendInput.argtypes = (wintypes.UINT, # nInputs
                              LPINPUT,       # pInputs
                              ctypes.c_int)  # cbSize
 
-
-KEY_NAMES = [
-    "\t",
-    "\n",
-    "\r",
-    " ",
-    "!",
-    '"',
-    "#",
-    "$",
-    "%",
-    "&",
-    "'",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    ":",
-    ";",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-    "[",
-    "\\",
-    "]",
-    "^",
-    "_",
-    "`",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "{",
-    "|",
-    "}",
-    "~",
-    "accept",
-    "add",
-    "alt",
-    "altleft",
-    "altright",
-    "apps",
-    "backspace",
-    "browserback",
-    "browserfavorites",
-    "browserforward",
-    "browserhome",
-    "browserrefresh",
-    "browsersearch",
-    "browserstop",
-    "capslock",
-    "clear",
-    "convert",
-    "ctrl",
-    "ctrlleft",
-    "ctrlright",
-    "decimal",
-    "del",
-    "delete",
-    "divide",
-    "down",
-    "end",
-    "enter",
-    "esc",
-    "escape",
-    "execute",
-    "f1",
-    "f10",
-    "f11",
-    "f12",
-    "f13",
-    "f14",
-    "f15",
-    "f16",
-    "f17",
-    "f18",
-    "f19",
-    "f2",
-    "f20",
-    "f21",
-    "f22",
-    "f23",
-    "f24",
-    "f3",
-    "f4",
-    "f5",
-    "f6",
-    "f7",
-    "f8",
-    "f9",
-    "final",
-    "fn",
-    "hanguel",
-    "hangul",
-    "hanja",
-    "help",
-    "home",
-    "insert",
-    "junja",
-    "kana",
-    "kanji",
-    "launchapp1",
-    "launchapp2",
-    "launchmail",
-    "launchmediaselect",
-    "left",
-    "modechange",
-    "multiply",
-    "nexttrack",
-    "nonconvert",
-    "num0",
-    "num1",
-    "num2",
-    "num3",
-    "num4",
-    "num5",
-    "num6",
-    "num7",
-    "num8",
-    "num9",
-    "numlock",
-    "pagedown",
-    "pageup",
-    "pause",
-    "pgdn",
-    "pgup",
-    "playpause",
-    "prevtrack",
-    "print",
-    "printscreen",
-    "prntscrn",
-    "prtsc",
-    "prtscr",
-    "return",
-    "right",
-    "scrolllock",
-    "select",
-    "separator",
-    "shift",
-    "shiftleft",
-    "shiftright",
-    "sleep",
-    "space",
-    "stop",
-    "subtract",
-    "tab",
-    "up",
-    "volumedown",
-    "volumemute",
-    "volumeup",
-    "win",
-    "winleft",
-    "winright",
-    "yen",
-    "command",
-    "option",
-    "optionleft",
-    "optionright",
-]
-KEYBOARD_KEYS = KEY_NAMES  # keeping old KEYBOARD_KEYS for backwards compatibility
-
-keyboardMapping = dict([(key, None) for key in KEY_NAMES])
-keyboardMapping.update({
-    'backspace': 0x08, # VK_BACK
-    '\b': 0x08, # VK_BACK
-    'super': 0x5B, #VK_LWIN
-    'tab': 0x09, # VK_TAB
-    '\t': 0x09, # VK_TAB
-    'clear': 0x0c, # VK_CLEAR
-    'enter': 0x0d, # VK_RETURN
-    '\n': 0x0d, # VK_RETURN
-    'return': 0x0d, # VK_RETURN
-    'shift': 0x10, # VK_SHIFT
-    'ctrl': 0x11, # VK_CONTROL
-    'alt': 0x12, # VK_MENU
-    'pause': 0x13, # VK_PAUSE
-    'capslock': 0x14, # VK_CAPITAL
-    'kana': 0x15, # VK_KANA
-    'hanguel': 0x15, # VK_HANGUEL
-    'hangul': 0x15, # VK_HANGUL
-    'junja': 0x17, # VK_JUNJA
-    'final': 0x18, # VK_FINAL
-    'hanja': 0x19, # VK_HANJA
-    'kanji': 0x19, # VK_KANJI
-    'esc': 0x1b, # VK_ESCAPE
-    'escape': 0x1b, # VK_ESCAPE
-    'convert': 0x1c, # VK_CONVERT
-    'nonconvert': 0x1d, # VK_NONCONVERT
-    'accept': 0x1e, # VK_ACCEPT
-    'modechange': 0x1f, # VK_MODECHANGE
-    ' ': 0x20, # VK_SPACE
-    'space': 0x20, # VK_SPACE
-    'pgup': 0x21, # VK_PRIOR
-    'pgdn': 0x22, # VK_NEXT
-    'pageup': 0x21, # VK_PRIOR
-    'pagedown': 0x22, # VK_NEXT
-    'end': 0x23, # VK_END
-    'home': 0x24, # VK_HOME
-    'left': 0x25, # VK_LEFT
-    'up': 0x26, # VK_UP
-    'right': 0x27, # VK_RIGHT
-    'down': 0x28, # VK_DOWN
-    'select': 0x29, # VK_SELECT
-    'print': 0x2a, # VK_PRINT
-    'execute': 0x2b, # VK_EXECUTE
-    'prtsc': 0x2c, # VK_SNAPSHOT
-    'prtscr': 0x2c, # VK_SNAPSHOT
-    'prntscrn': 0x2c, # VK_SNAPSHOT
-    'printscreen': 0x2c, # VK_SNAPSHOT
-    'insert': 0x2d, # VK_INSERT
-    'del': 0x2e, # VK_DELETE
-    'delete': 0x2e, # VK_DELETE
-    'help': 0x2f, # VK_HELP
-    'win': 0x5b, # VK_LWIN
-    'winleft': 0x5b, # VK_LWIN
-    'winright': 0x5c, # VK_RWIN
-    'apps': 0x5d, # VK_APPS
-    'sleep': 0x5f, # VK_SLEEP
-    'num0': 0x60, # VK_NUMPAD0
-    'num1': 0x61, # VK_NUMPAD1
-    'num2': 0x62, # VK_NUMPAD2
-    'num3': 0x63, # VK_NUMPAD3
-    'num4': 0x64, # VK_NUMPAD4
-    'num5': 0x65, # VK_NUMPAD5
-    'num6': 0x66, # VK_NUMPAD6
-    'num7': 0x67, # VK_NUMPAD7
-    'num8': 0x68, # VK_NUMPAD8
-    'num9': 0x69, # VK_NUMPAD9
-    'multiply': 0x6a, # VK_MULTIPLY  ??? Is this the numpad *?
-    'add': 0x6b, # VK_ADD  ??? Is this the numpad +?
-    'separator': 0x6c, # VK_SEPARATOR  ??? Is this the numpad enter?
-    'subtract': 0x6d, # VK_SUBTRACT  ??? Is this the numpad -?
-    'decimal': 0x6e, # VK_DECIMAL
-    'divide': 0x6f, # VK_DIVIDE
-    'f1': 0x70, # VK_F1
-    'f2': 0x71, # VK_F2
-    'f3': 0x72, # VK_F3
-    'f4': 0x73, # VK_F4
-    'f5': 0x74, # VK_F5
-    'f6': 0x75, # VK_F6
-    'f7': 0x76, # VK_F7
-    'f8': 0x77, # VK_F8
-    'f9': 0x78, # VK_F9
-    'f10': 0x79, # VK_F10
-    'f11': 0x7a, # VK_F11
-    'f12': 0x7b, # VK_F12
-    'f13': 0x7c, # VK_F13
-    'f14': 0x7d, # VK_F14
-    'f15': 0x7e, # VK_F15
-    'f16': 0x7f, # VK_F16
-    'f17': 0x80, # VK_F17
-    'f18': 0x81, # VK_F18
-    'f19': 0x82, # VK_F19
-    'f20': 0x83, # VK_F20
-    'f21': 0x84, # VK_F21
-    'f22': 0x85, # VK_F22
-    'f23': 0x86, # VK_F23
-    'f24': 0x87, # VK_F24
-    'numlock': 0x90, # VK_NUMLOCK
-    'scrolllock': 0x91, # VK_SCROLL
-    'shiftleft': 0xa0, # VK_LSHIFT
-    'shiftright': 0xa1, # VK_RSHIFT
-    'ctrlleft': 0xa2, # VK_LCONTROL
-    'ctrlright': 0xa3, # VK_RCONTROL
-    'altleft': 0xa4, # VK_LMENU
-    'altright': 0xa5, # VK_RMENU
-    'browserback': 0xa6, # VK_BROWSER_BACK
-    'browserforward': 0xa7, # VK_BROWSER_FORWARD
-    'browserrefresh': 0xa8, # VK_BROWSER_REFRESH
-    'browserstop': 0xa9, # VK_BROWSER_STOP
-    'browsersearch': 0xaa, # VK_BROWSER_SEARCH
-    'browserfavorites': 0xab, # VK_BROWSER_FAVORITES
-    'browserhome': 0xac, # VK_BROWSER_HOME
-    'volumemute': 0xad, # VK_VOLUME_MUTE
-    'volumedown': 0xae, # VK_VOLUME_DOWN
-    'volumeup': 0xaf, # VK_VOLUME_UP
-    'nexttrack': 0xb0, # VK_MEDIA_NEXT_TRACK
-    'prevtrack': 0xb1, # VK_MEDIA_PREV_TRACK
-    'stop': 0xb2, # VK_MEDIA_STOP
-    'playpause': 0xb3, # VK_MEDIA_PLAY_PAUSE
-    'launchmail': 0xb4, # VK_LAUNCH_MAIL
-    'launchmediaselect': 0xb5, # VK_LAUNCH_MEDIA_SELECT
-    'launchapp1': 0xb6, # VK_LAUNCH_APP1
-    'launchapp2': 0xb7, # VK_LAUNCH_APP2
-    })
-
-# Populate the basic printable ascii characters.
-# https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-vkkeyscana
-for c in range(32, 128):
-    keyboardMapping[chr(c)] = ctypes.windll.user32.VkKeyScanA(ctypes.wintypes.WCHAR(chr(c)))
+keyboardMapping = {
+'\t ': 0x09,
+'\n': 0x0d,
+'\r': 0xd,
+' ' : 0x20,
+'!' : 0x131,
+'"' : 0x1de,
+'#' : 0x133,
+'$' : 0x134,
+'%' : 0x135,
+'&' : 0x137,
+"'" : 0xde,
+'(' : 0x139,
+')' : 0x130,
+'*' : 0x138,
+'+' : 0x1bb,
+',' : 0xbc,
+'-' : 0xbd,
+'.' : 0xbe,
+'/' : 0xbf,
+'0' : 0x30,
+'1' : 0x31,
+'2' : 0x32,
+'3' : 0x33,
+'4' : 0x34,
+'5' : 0x35,
+'6' : 0x36,
+'7' : 0x37,
+'8' : 0x38,
+'9' : 0x39,
+':' : 0x1ba,
+';' : 0xba,
+'<' : 0x1bc,
+'=' : 0xbb,
+'>' : 0x1be,
+'?' : 0x1bf,
+'@' : 0x132,
+'[' : 0xdb,
+'\\' : 0xdc,
+']' : 0xdd,
+'^' : 0x136,
+'_' : 0x1bd,
+'`' : 0xc0,
+'a' : 0x41,
+'b' : 0x42,
+'c' : 0x43,
+'d' : 0x44,
+'e' : 0x45,
+'f' : 0x46,
+'g' : 0x47,
+'h' : 0x48,
+'i' : 0x49,
+'j' : 0x4a,
+'k' : 0x4b,
+'l' : 0x4c,
+'m' : 0x4d,
+'n' : 0x4e,
+'o' : 0x4f,
+'p' : 0x50,
+'q' : 0x51,
+'r' : 0x52,
+'s' : 0x53,
+'t' : 0x54,
+'u' : 0x55,
+'v' : 0x56,
+'w' : 0x57,
+'x' : 0x58,
+'y' : 0x59,
+'z' : 0x5a,
+'{' : 0x1db,
+'|' : 0x1dc,
+'}' : 0x1dd,
+'~' : 0x1c0,
+'accept' : 0x1e,
+'add' : 0x6b,
+'alt' : 0x12,
+'altleft' : 0xa4,
+'altright' : 0xa5,
+'apps' : 0x5d,
+'backspace' : 0x8,
+'browserback' : 0xa6,
+'browserfavorites' : 0xab,
+'browserforward' : 0xa7,
+'browserhome' : 0xac,
+'browserrefresh' : 0xa8,
+'browsersearch' : 0xaa,
+'browserstop' : 0xa9,
+'capslock' : 0x14,
+'clear' : 0xc,
+'convert' : 0x1c,
+'ctrl' : 0x11,
+'ctrlleft' : 0xa2,
+'ctrlright' : 0xa3,
+'decimal' : 0x6e,
+'del' : 0x2e,
+'delete' : 0x2e,
+'divide' : 0x6f,
+'down' : 0x28,
+'end' : 0x23,
+'enter' : 0xd,
+'esc' : 0x1b,
+'escape' : 0x1b,
+'execute' : 0x2b,
+'f1' : 0x70,
+'f2' : 0x71,
+'f3' : 0x72,
+'f4' : 0x73,
+'f5' : 0x74,
+'f6' : 0x75,
+'f7' : 0x76,
+'f8' : 0x77,
+'f9' : 0x78,
+'f10' : 0x79,
+'f11' : 0x7a,
+'f12' : 0x7b,
+'f13' : 0x7c,
+'f14' : 0x7d,
+'f15' : 0x7e,
+'f16' : 0x7f,
+'f17' : 0x80,
+'f18' : 0x81,
+'f19' : 0x82,
+'f20' : 0x83,
+'f21' : 0x84,
+'f22' : 0x85,
+'f23' : 0x86,
+'f24' : 0x87,
+'final' : 0x18,
+'fn' : None,
+'hanguel' : 0x15,
+'hangul' : 0x15,
+'hanja' : 0x19,
+'help' : 0x2f,
+'home' : 0x24,
+'insert' : 0x2d,
+'junja' : 0x17,
+'kana' : 0x15,
+'kanji' : 0x19,
+'launchapp1' : 0xb6,
+'launchapp2' : 0xb7,
+'launchmail' : 0xb4,
+'launchmediaselect' : 0xb5,
+'left' : 0x25,
+'modechange' : 0x1f,
+'multiply' : 0x6a,
+'nexttrack' : 0xb0,
+'nonconvert' : 0x1d,
+'num0' : 0x60,
+'num1' : 0x61,
+'num2' : 0x62,
+'num3' : 0x63,
+'num4' : 0x64,
+'num5' : 0x65,
+'num6' : 0x66,
+'num7' : 0x67,
+'num8' : 0x68,
+'num9' : 0x69,
+'numlock' : 0x90,
+'pagedown' : 0x22,
+'pageup' : 0x21,
+'pause' : 0x13,
+'pgdn' : 0x22,
+'pgup' : 0x21,
+'playpause' : 0xb3,
+'prevtrack' : 0xb1,
+'print' : 0x2a,
+'printscreen' : 0x2c,
+'prntscrn' : 0x2c,
+'prtsc' : 0x2c,
+'prtscr' : 0x2c,
+'return' : 0xd,
+'right' : 0x27,
+'scrolllock' : 0x91,
+'select' : 0x29,
+'separator' : 0x6c,
+'shift' : 0x10,
+'shiftleft' : 0xa0,
+'shiftright' : 0xa1,
+'sleep' : 0x5f,
+'space' : 0x20,
+'stop' : 0xb2,
+'subtract' : 0x6d,
+'tab' : 0x9,
+'up' : 0x26,
+'volumedown' : 0xae,
+'volumemute' : 0xad,
+'volumeup' : 0xaf,
+'win' : 0x5b,
+'winleft' : 0x5b,
+'winright' : 0x5c,
+'yen' : None,
+'command' : None,
+'option' : None,
+'optionleft' : None,
+'optionright' : None,
+'\b': 0x08,
+'super' : 0x5b,
+'A' : 0x141,
+'B' : 0x142,
+'C' : 0x143,
+'D' : 0x144,
+'E' : 0x145,
+'F' : 0x146,
+'G' : 0x147,
+'H' : 0x148,
+'I' : 0x149,
+'J' : 0x14a,
+'K' : 0x14b,
+'L' : 0x14c,
+'M' : 0x14d,
+'N' : 0x14e,
+'O' : 0x14f,
+'P' : 0x150,
+'Q' : 0x151,
+'R' : 0x152,
+'S' : 0x153,
+'T' : 0x154,
+'U' : 0x155,
+'V' : 0x156,
+'W' : 0x157,
+'X' : 0x158,
+'Y' : 0x159,
+'Z' : 0x15a,
+'\x7f' : 0x208}
 
 def isShiftCharacter(character):
     """
